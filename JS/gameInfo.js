c3f68bbc59e63c4af8842ae8dcc0b4459169dd88
Emad Jammal <emad.elqassas.1@gmail.com>
2023-08-31 03:53:03 +0300

    modification

--- JS/gameInfo.js
+++ JS/gameInfo.js
@@ -1,63 +1,39 @@
 export class GameInfo{
-  constructor(results){
-    this.result = results
-    console.log(this.result);
-    this.display()
+  constructor(id){
+    this.getDetails(id)
   }
-  display() {
-    let game = "";
-    for (let i = 0; i < this.result.length; i++) {
-      game += `
-      <div class="box">
-      <div class="border border-dark border-1 p-3 h-100 ">
-      <div class="card bg-transparent text-white d-flex flex-column border-0 justify-content-between align-items-center">
-        <img src="${this.result[i].thumbnail}" class="card-img-top">
-        <div class="card-body border-0">
-          <div class="d-flex justify-content-between align-items-center">
-          <h5 class="card-title">${this.result[i].title}</h5>
-          <p id="free" class="btn btn-primary">FREE</p>
-          </div>
-          <p class="card-text fs-6 text-gray">${this.result[i].short_description}</p>
-          <p class="link d-none">${this.result[i].game_url}</p>
-        </div>
-        <div class="card-body border-1 border-top border-black d-flex justify-content-between align-items-center pt-3">
-          <a href="#" class="card-link text-white nav-link bg-black bg-opacity-25 px-2 py-1 h-fit rounded-3 category">${this.result[i].genre}</a>
-          <a href="#" class="card-link text-white nav-link bg-black bg-opacity-25 px-2 py-1 h-fit rounded-3 platform">${this.result[i].platform}</a>
-        </div>
-      </div>
-    </div>
-    </div>
-    `
+
+async getDetails(id){
+  $(".game-info-loading-page").removeClass("d-none");
+  const url = `https://free-to-play-games-database.p.rapidapi.com/api/game?id=${id}`;
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'e871f3d194msh4a231076c4f1818p1d8de4jsnbd68ebe35be7',
+      'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
     }
-    $("#row").html(game);
-    this.setDataToDetails()
-  }
-  setDataToDetails(){
-    let boxes = document.querySelectorAll(".box");
-    for (let i = 0; i < boxes.length; i++) {
-      $(boxes[i]).click(function () {
-        $("#gameInfo").removeClass("d-none");
-        let imgBox = this.querySelector("img").getAttribute("src");
-        let titleBox = this.querySelector(".card-title").innerHTML;
-        let discBox = this.querySelector(".card-text").innerHTML;
-        let platformBox = this.querySelector(".platform").innerHTML;
-        let categoryBox = this.querySelector(".category").innerHTML;
-        let linkBox = this.querySelector(".link").innerHTML
-        $(".imgInfo").attr("src",imgBox)
-        $(".Category").html(categoryBox);
-        $(".Platform").html(platformBox);
-        $(".discription").html(discBox)
-        $(".title").html(titleBox)
-        $(".btnLink").attr("href",linkBox);
-        setTimeout(function(){
-          $(".game-info-loading-page").addClass("d-none");
-        },300)
-      })
-    }
-     // CLOSE GAME INFO
-      $(".close").click(()=>{
-        $("#gameInfo").addClass("d-none")
-      })
-  }
-  
+  };
+
+  const response = await fetch(url, options);
+  const result = await response.json();
+  $(".game-info-loading-page").addClass("d-none");
+  this.displayDetails(result);
+
+}
+
+
+displayDetails(result){
+  const detailsBox = `
+  <img class="imgInfo rounded img-fluid flex-basis-40" src="${result.thumbnail}" alt="">
+  <div class="infoData flex-basis-60">
+    <h4 class="title">${result.title}</h4>
+    <div class="d-flex align-items-center"><p class="me-2">Category: </p> <p class="Category btn btn-info mb-3 p-0 px-2">${result.genre}</p></div>
+    <div class="d-flex align-items-center"><p class="me-2">Platform: </p> <p class="Platform btn btn-info mb-3 p-0 px-2">${result.platform}</p></div>
+    <div class="d-flex align-items-center"><p class="me-2">Statue: </p> <p class="Status btn btn-info p-0 px-2 mb-3 ">${result.status}</p></div>
+    <div class="mb-5"><p class="mb-1">Discription: </p><p class="discription">${result.description}</p></div>
+    <a class="btnLink btn btn-outline-warning text-white" target="_blank" href="${result.game_url}">Show Game</a>
+  </div>
+  `
+  $(".info").html(detailsBox);
+}
 }
--- JS/games.js
+++ JS/games.js
@@ -36,9 +36,47 @@ export class Games {
     const response = await fetch(this.url, this.options);
     // CONVERT FROM STRINGIFY TO PARSE
     this.result = await response.json();
-    const games = new GameInfo(this.result);
+    this.display(this.result)
+    console.log('result: ', this.result);
+
     $(".loading-page").addClass("d-none");
   }
+  display(result) {
+    let game = "";
+    for (let i = 0; i < result.length; i++) {
+      game += `
+      <div class="box" data-id="${result[i].id}">
+      <div class="border border-dark border-1 p-3 h-100 ">
+      <div class="card bg-transparent text-white d-flex flex-column border-0 justify-content-between align-items-center">
+        <img src="${result[i].thumbnail}" class="card-img-top">
+        <div class="card-body border-0">
+          <div class="d-flex justify-content-between align-items-center">
+          <h5 class="card-title">${result[i].title}</h5>
+          <p id="free" class="btn btn-primary">FREE</p>
+          </div>
+          <p class="card-text fs-6 text-gray">${result[i].short_description}</p>
+        </div>
+        <div class="card-body border-1 border-top border-black d-flex justify-content-between align-items-center flex-0 pt-3">
+          <a href="#" class="card-link text-white nav-link bg-black bg-opacity-25 px-2 py-1 h-fit rounded-3 category">${result[i].genre}</a>
+          <a href="#" class="card-link text-white nav-link bg-black bg-opacity-25 px-2 py-1 h-fit rounded-3 platform">${result[i].platform}</a>
+        </div>
+      </div>
+    </div>
+    </div>
+    ` 
+    $("#row").html(game);
+    document.querySelectorAll(".box").forEach((box) => {
+      box.addEventListener("click",()=>{
+        $("#gameInfo").removeClass("d-none");
+        const info = new GameInfo(box.dataset.id);
+      })
+            $(".close").click(()=>{
+        $("#gameInfo").addClass("d-none")
+      })
+    })
+  }
+
+}
 
 
 }
